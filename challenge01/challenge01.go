79b70c77a80ac972361cc5ef2986056f692c779d
jemprojects <jemprojectsgit@gmail.com>
2023-12-08 20:11:37 -0300

    arreglo challenge1

--- challenge01/challenge01.go
+++ challenge01/challenge01.go
@@ -16,17 +16,17 @@ func findFirstRepeated(gifts []int) int {
 }
 func main() {
 	giftIds := []int{2, 1, 3, 5, 3, 2}
-	firstRepeatedId := findFirstRepeated(giftIds)
-	fmt.Println(firstRepeatedId) // 3
+	result := findFirstRepeated(giftIds)
+	fmt.Println(result) // 3
 	// Aunque el 2 y el 3 se repiten
 	// el 3 aparece primero por segunda vez
 
-	giftIds2 := []int{1, 2, 3, 4}
-	firstRepeatedId2 := findFirstRepeated(giftIds2)
-	fmt.Println(firstRepeatedId2) // -1
+	giftIds = []int{1, 2, 3, 4}
+	result = findFirstRepeated(giftIds)
+	fmt.Println(result) // -1
 	// Es -1 ya que no se repite ningún número
 
-	giftIds3 := []int{5, 1, 5, 1}
-	firstRepeatedId3 := findFirstRepeated(giftIds3)
-	fmt.Println(firstRepeatedId3) // 5
+	giftIds = []int{5, 1, 5, 1}
+	result = findFirstRepeated(giftIds)
+	fmt.Println(result) // 5
 }
